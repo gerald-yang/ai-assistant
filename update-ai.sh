b72d54fd3f3526af35fa4bac92c487d5c5151d71
Gerald Yang <gerald.yang@canonical.com>
2026-02-13 10:22:36 +0800

    update script

--- update-ai.sh
+++ update-ai.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-claude update
-npm i -g @openai/codex
-opencode upgrade
+if command -v "claude" >/dev/null 2>&1; then
+	claude update
+else
+	echo "claud not installed"
+fi
+
+
+if command -v "codex" >/dev/null 2>&1; then
+	npm i -g @openai/codex
+else
+	echo "codex not installed"
+fi
+
+if command -v "opencode" >/dev/null 2>&1; then
+	opencode upgrade
+else
+	echo "opencode not install"
+fi
