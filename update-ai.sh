905fab14f68017988dc0a6ff2013e6f34e3747dc
Gerald Yang <gerald.yang@canonical.com>
2026-06-02 09:47:25 +0800

    tweak folder and logs

--- update-ai.sh
+++ update-ai.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
+echo ""
 if command -v "claude" >/dev/null 2>&1; then
-	echo "update claude code"
+	echo "### update claude code ###"
 	claude update
 else
 	echo "claud not installed"
 fi
 
+echo ""
 if command -v "codex" >/dev/null 2>&1; then
-	echo "update codex"
+	echo "## update codex ##"
 	npm i -g @openai/codex
 else
 	echo "codex not installed"
 fi
 
+echo ""
 if command -v "opencode" >/dev/null 2>&1; then
-	echo "update opencode"
+	echo "## update opencode ##"
 	opencode upgrade
 else
 	echo "opencode not install"
 fi
 
+echo ""
 if command -v "agy" >/dev/null 2>&1; then
-	echo "update antigravity cli"
+	echo "## update antigravity cli ##"
 	agy update
 else
 	echo "agy not install"
